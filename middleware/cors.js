f60545e6d5ac7b64365f60f8cc7e2a6e564163d7
Ryan Harmuth <ryanharmuth@gmail.com>
2016-08-15 11:49:44 +0700

    Cors test. Getting an issue on live

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@
 
 var cors = function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, UserId");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, UserId, PewPew");
 	next();
 }
 
